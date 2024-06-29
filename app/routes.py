c9ccb0a1c1594d2afa52a8c9827a088f11f04603
zlamprich <zachary.lamprich@outlook.com>
2024-06-28 23:23:42 -0600

    Initial repo creation

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+
+@app.route('/index')
+def index():
+    user = {'username': 'Zach'}
+    posts = [
+        {
+            'author': {'username': 'Zach'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Kristi'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
