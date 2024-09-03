77d64e919ca37e4ea0c3d511b5dbc83e7e5498bd
zlamprich-dev <zlamprich.dev@gmail.com>
2024-09-03 12:03:20 -0600

    Route additions, index template edits for blog posts

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username": "Zach"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+    ]
+
+    return render_template("index.html", title="Home", user=user, posts=posts)
